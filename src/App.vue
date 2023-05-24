663dc0d9fed7e76ecde4d859c72812346d3bd749
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-24 16:27:17 +0200

    add mykey in store.js

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppMainContent from "./components/AppMainContent.vue";
+import axios from "axios";
+import { store } from "./store";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
   components: {
     AppHeader,
     AppMainContent,
+  },
+  methods: {
+    requestDataFromApi() {
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            name: this.store.searchStr,
+            status: this.store.searchStatus,
+          },
+        })
+        .then((response) => (this.store.characterList = response.data.results));
+    },
   },
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -2,4 +2,5 @@ import { reactive } from "vue";
 
 export const store = reactive({
   filmList: [],
+  myKey: "b94e84e10d77f413fe8fd376740d6ee5",
 });
